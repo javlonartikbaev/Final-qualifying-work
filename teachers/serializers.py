655942a35879ff3e7a95d50aaa49d3832e38a663
Artikbaev Javlon <artjavlon@gmail.com>
2025-04-03 23:05:33 +0500

    fixed bug(update teacher)

--- teachers/serializers.py
+++ teachers/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from teachers.models import Teacher, PositionTeacher
@@ -19,3 +20,14 @@ class TeacherSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return Teacher.objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        position_id = validated_data.pop('position_id', instance.position_id)
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.position_id = position_id
+        instance.save()
+        return instance
